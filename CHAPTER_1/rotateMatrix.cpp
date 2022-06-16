619707f0edc501d88f99358fe7096f67754b99d9
Chris <chris.l.bryant@gmail.com>
2022-06-15 23:37:44 -0500

    Added code to rotate matrix

--- CHAPTER_1/rotateMatrix.cpp
+++ CHAPTER_1/rotateMatrix.cpp
@@ -25,6 +25,7 @@ int main() {
 int array1[N][N];
 int array2[N][N];
 
+// populating the first array with a random number generator
 for (int i = 0; i < N; i++) {
 	for (int j = 0; j < N; j++) {
 		array1[i][j] = rand() % 100;
@@ -35,9 +36,10 @@ cout << "Matrix before rotation" << endl;
 cout << "----------------------" << endl;
 printMatrix(array1);
 
+// rotating matrix and assigning to a new array
 for (int i = 0; i < N; i++) {
-	for (int j = N - 1; j <= 0 ; j--) {
-		array2[i][j] = array1[i][j];
+	for (int j = 0; j < N ; j++) {
+		array2[i][j] = array1[N - 1 - j][i];
 	}
 }
 
