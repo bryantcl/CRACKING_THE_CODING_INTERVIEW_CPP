63e73f0b761887c157d22cec43c6e22f5c64b2f2
Chris <chris.l.bryant@gmail.com>
2022-06-15 23:02:46 -0500

    made changes to autopopulate array and print array

--- CHAPTER_1/rotateMatrix.cpp
+++ CHAPTER_1/rotateMatrix.cpp
@@ -4,11 +4,46 @@ write a method to rotate the image by 90 degrees.
 */
 
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 #define N 5
 
+void printMatrix(int a[N][N]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 int main() {
+
+int array1[N][N];
+int array2[N][N];
+
+for (int i = 0; i < N; i++) {
+	for (int j = 0; j < N; j++) {
+		array1[i][j] = rand() % 100;
+	}
+}
+
+cout << "Matrix before rotation" << endl;
+cout << "----------------------" << endl;
+printMatrix(array1);
+
+for (int i = 0; i < N; i++) {
+	for (int j = N - 1; j <= 0 ; j--) {
+		array2[i][j] = array1[i][j];
+	}
+}
+
+cout << "Matrix after rotation" << endl;
+cout << "---------------------" << endl;
+printMatrix(array2);
 
 return 0;
 }
